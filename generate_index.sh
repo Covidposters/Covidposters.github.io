7e426ed41fc50eab130f80f0e3bef07c4a10c8d3
kimberley lawrence <kimberleynedic@gmail.com>
2020-03-26 00:19:16 +1100

    Added in PDF, PNG, TXT and Ai Files

---
+++ generate_index.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cat header.html
+
+for IMG_PATH in $(find images -type f -name \*.ai | sed 's/.ai//g' );do
+
+TEXT_TITLE=$(cat ${IMG_PATH}.txt | head -1)
+TEXT_BODY=$(cat ${IMG_PATH}.txt | tail -1)
+
+cat << EOF
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+	    <a href="${IMG_PATH}.pdf">
+	      <img src="${IMG_PATH}.png" class="card-img-top" focusable="false" role="img">
+	  </a>
+              <div class="card-body">
+              <p class="card-text">
+		<h3>${TEXT_TITLE}</h3>
+	        <p>${TEXT_BODY}</p>
+	      </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+			<a href="${IMG_PATH}.pdf" class="btn btn-sm btn-outline-secondary" type="button">pdf</a>
+			<a href="${IMG_PATH}.png" class="btn btn-sm btn-outline-secondary" type="button">png</a>
+			<a href="${IMG_PATH}.ai" class="btn btn-sm btn-outline-secondary" type="button">ai (source)</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+EOF
+
+done
